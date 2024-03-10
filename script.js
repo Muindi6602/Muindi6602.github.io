8646849d69138175ad974eb418d306d1e9d70493
Muindi6602 <140938393+Muindi6602@users.noreply.github.com>
2024-03-10 22:17:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -71,3 +71,17 @@ $(document).ready(function(){
         }
     });
 });
+// Smooth scrolling for anchor links
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+});
+
+
